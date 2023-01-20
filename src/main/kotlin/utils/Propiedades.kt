9400798c06a3d38d93b081a4a770146a7609e616
Sebastián Mendoza Acosta <jhoansebastian.mendoza@alumno.iesluisvives.org>
2023-01-20 16:31:37 +0100

    Método para leer properties y configuración inicial de archivo de propiedades

---
+++ src/main/kotlin/utils/Propiedades.kt
@@ -0,0 +1,27 @@
+package utils
+
+/**
+ * @author Mario Resa y Sebastián Mendoza
+ */
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
+/**
+ * Clase de tipo Object que contiene el método para leer archivos Properties()
+ */
+object Propiedades {
+    /**
+     * Método genérico que lee propiedades de un archivo Properties()
+     * @param String Nombre del archivo a leer
+     * @return Devuelve el archivo especificado para obtener los datos deseados
+     */
+    fun propertiesReader(propertiesFile: String): Properties {
+        val raiz = System.getProperty("user.dir")
+        val file =
+            FileInputStream(raiz + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + propertiesFile)
+        val prop = Properties()
+        prop.load(file)
+        return prop
+    }
+}
